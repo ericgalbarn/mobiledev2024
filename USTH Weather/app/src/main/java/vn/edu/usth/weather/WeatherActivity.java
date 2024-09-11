67014634d038508f2d703c841b659d9e51af481b
ericgalbarn <ericgalbarn@gmail.com>
2024-09-11 17:08:19 +0700

    Labword 7+8 Update

--- USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,56 +1,41 @@
 package vn.edu.usth.weather;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class WeatherActivity extends AppCompatActivity {
-    private static final String TAG = "WeatherActivity";
+
+    private ViewPager2 viewPager;
+    private WeatherPagerAdapter pagerAdapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        Log.i(TAG, "onCreate() called");
 
-        // Add WeatherFragment
-        WeatherFragment weatherFragment = new WeatherFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.weather_fragment, weatherFragment).commit();
+        // Setup ViewPager and TabLayout
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
 
-        // Add ForecastFragment
-        ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.container, firstFragment).commit();
-    }
+        pagerAdapter = new WeatherPagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart() called");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume() called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause() called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop() called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy() called");
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Hanoi, Vietnam");
+                    break;
+                case 1:
+                    tab.setText("Paris, France");
+                    break;
+                case 2:
+                    tab.setText("Toulouse, France");
+                    break;
+            }
+        }).attach();
     }
 }
---
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherPagerAdapter.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.weather;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class WeatherPagerAdapter extends FragmentStateAdapter {
+
+    public WeatherPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return new WeatherAndForecastFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
